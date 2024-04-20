6c4ed7481994406dda1fd182ec6be4cdaa9ec667
iv4n42 <ivanernand42@gmail.com>
2024-04-20 12:37:48 -0400

    seting post creation

--- index.js
+++ index.js
@@ -8,13 +8,18 @@ let posts = [
     {user : 'Marco' , title : 'huevo' , description  : 'text3'}
 ]
 
-app.use(cors())
+app.use(cors(),express.urlencoded({extended : true}),express.json())
+
 
 app.get('/',(req,res)=>{
     res.send('empezando papa')
 })
 app.get('/posts',(req,res)=>{
     res.send(posts)
+})
+app.post('/posts',(req,res) => {
+    posts.push(req.body)
+    res.send("Se hizo tu post")
 })
 app.listen(3000,() => {
     console.log('hello world')
