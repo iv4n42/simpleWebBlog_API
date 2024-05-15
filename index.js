7a16eb97931e51dd07816a27d414c69101b31f3d
iv4n42 <ivanernand42@gmail.com>
2024-05-15 19:16:07 -0400

    Adding mongose and connecting with mongo

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
 
-let posts = [
-    {user : 'Ivan' , title : 'azuca' , description  : 'text1'},
-    {user : 'Pedro' , title : 'merengue' , description  : 'text2'},
-    {user : 'Marco' , title : 'huevo' , description  : 'text3'}
-]
 
-app.use(cors(),express.urlencoded({extended : true}),express.json())
+
+async function connectPosts() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/posts');
+  
+  }
+connectPosts().then(() => console.log('postconnect sirvio')).catch(e => console.log(e));
+const postSchema =  new mongoose.Schema({
+    user : String,
+    title : String,
+    description : String,
+    text : String,
+});
+const Post = mongoose.model('Post',postSchema);
+let posts = []
+
+
+app.use(cors(),express.urlencoded({extended : true}),express.json());
 
 
 app.get('/',(req,res)=>{
     res.send('empezando papa')
 })
 app.get('/posts',(req,res)=>{
-    res.send(posts)
+    Post.find({}).then(data => {
+        posts = data
+        res.send(posts)
+    })
+    
 })
 app.post('/posts',(req,res) => {
-    posts.push(req.body)
-    res.send("Se hizo tu post")
+    const newPost = new Post(req.body)
+    newPost.save().then(()=> res.send("Se hizo tu post"))
 })
 app.listen(3000,() => {
     console.log('hello world')
